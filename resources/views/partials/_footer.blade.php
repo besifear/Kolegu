715243151a028d62a826a67a88989538ee236ac5
Taulant Shatri <ts36599@ubt-uni.net>
2016-12-20 14:04:49 +0100

    footer updated

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,9 +1,35 @@
-		<footer>
-         <div class="container">
+<footer class="text-muted well text-center">
+	<div class="col-lg-2">
+		<a href="#" class="pull-left">About Us</a><br>
+		<a href="#" data-toggle="modal" data-target="#suggestionModal" class="pull-left">Send suggestions</a>
+		<div class="modal fade" id="suggestionModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+		  <div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+					<h4 class="modal-title" id="lineModalLabel">Send Suggestion</h4>
+				</div>
+				<div class="modal-body">
 
-            <div class="copy text-center">
-               Copyright 2016 <a href='#'>Kolegu</a>
-            </div>
+					<!-- content goes here -->
+					<form>
+					  <div class="form-group">
+						<label for="exampleInputEmail1">Subject</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" placeholder="">
+					  </div>
+					  <div class="form-group">
+						<label for="exampleInputPassword1">Suggestion</label>
+						<textarea rows="5" class="form-control" id="exampleInputPassword1" placeholder=""></textarea>
+					  </div>
+					  <button type="submit" class="btn btn-default">Submit</button>
+					</form>
 
-         </div>
-      	</footer>
+				</div>
+			</div>
+		  </div>
+		</div>
+	</div><br><br>
+	<div class="col-lg-10">
+		<p class="col-lg-offset-1">Copyright <a href="#">Kolegu</a></p>
+	</div><br>
+</footer>
