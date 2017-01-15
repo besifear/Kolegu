ab5b2d0eb996df7c63bd99233300a3af3038047c
besifear <hackerofheaven@hotmail.com>
2017-01-15 13:47:05 +0100

    Dergimi i mesazheve

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'koleguqa@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'KoleguQ&A'),
     ],
 
     /*
@@ -111,5 +111,7 @@ return [
     */
 
     'sendmail' => '/usr/sbin/sendmail -bs',
+
+    'pretend'=>false,
 
 ];
