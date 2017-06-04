99cb2384bcaf711f651831df7008692dd5503673
besifear <hackerofheaven@hotmail.com>
2017-06-04 11:34:12 +0200

    Removed Vendor from git

--- vendor/albertcht/invisible-recaptcha/src/config/captcha.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'siteKey' => env('INVISIBLE_RECAPTCHA_SITEKEY'),
-    'secretKey' => env('INVISIBLE_RECAPTCHA_SECRETKEY'),
-    'hideBadge' => env('INVISIBLE_RECAPTCHA_BADGEHIDE', false)
-];
