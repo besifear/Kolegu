592ac4e584346ffcbd9305499e45fef0300d121b
Besnik Korca <besnik.korca1@gmail.com>
2017-12-13 21:22:47 +0100

    Optimization of Question Search

--- resources/assets/js/components/Search.vue
+++ resources/assets/js/components/Search.vue
@@ -9,14 +9,6 @@
 	                  </button>
 	              </div>
 	          </div>
-
-	          <div id="livesearchcontainer">
-	         	<ul v-show="hasSearchResult" id="search-result-list" class="search-result-list" style="list-style: none;">
-	         		<li><h4> Pyetje </h4></li>
-					<search-result-item v-for="item in items" v-bind:key="item.id" v-bind:item="item">
-					</search-result-item>
-                </ul>
-	          </div>
 	</div>
 </template>
 <script>
@@ -26,26 +18,7 @@
 		},
 		data(){
 			return {
-				query: "",
-				items: [],
 				index: {}
-			}
-		},
-		methods:{
-			search(){
-			   	if( this.query.length == 0 ){
-			   		this.items= [];	
-			   	}else{
-			   		this.index.search( this.query, function( error, results ){
-		           		this.items = results.hits;
-		        	}.bind(this));
-			   	} 
-	        }
-
-		},
-		computed: {
-			hasSearchResult(){
-				return this.items.length
 			}
 		},
 		mounted(){
